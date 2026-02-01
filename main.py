d6e109f49e967730b7a1cbcf0f7141a0bfafed5e
Fraulein Alfa C. Lacson <100470332+frauleinclacson@users.noreply.github.com>
2026-02-01 16:03:01 +0000

    main.py update

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ import sys
 import os
 from core.game_engine import GameEngine
 from config.game_config import GameConfig
+
+def main():
+    pygame.init()
+
+    #Game initialization
+    config = GameConfig()
+    game = GameEngine(config)
+
+    #Loop of the game
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        dt = clock.tick(config.FPS) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            game.handle_event(event)
+
+        game.update(dt)
+        game.render()
+
+        pygame.display.flip()
+
+    pygame.quit ()
+    sys.exit ()
+if __name__ == "__My World Main__"
+    main()
