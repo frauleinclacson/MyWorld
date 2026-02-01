afbd46d9fe283b4e03e4bfa5f411968597df7514
Fraulein Alfa C. Lacson <100470332+frauleinclacson@users.noreply.github.com>
2026-02-01 16:12:20 +0000

    engine

--- Core/game_engine.py
+++ Core/game_engine.py
@@ -4,3 +4,7 @@ from core.scene_manager import SceneManager
 from world.world_manager import WorldManager
 from characters.character_manager import ChharacterManager
 from ui.main_menu import MainMenu
+from monetization.package_manager import PackageManager
+
+class GameEngine:
+    
